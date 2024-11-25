9e6c754ea7828a30a396fe3d68a99ed30292df20
MaudyDhiya <maudydhia@gmail.com>
2024-11-25 12:06:00 +0700

    final result

--- Web_Transportation_Problem_Project/asgi.py
+++ Web_Transportation_Problem_Project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for web_skripsi project.
+ASGI config for Web Transportational Problem Project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- Web_Transportation_Problem_Project/wsgi.py
+++ Web_Transportation_Problem_Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for web_skripsi project.
+WSGI config for Web Transportational Problem Project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
